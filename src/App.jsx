af7fa1d8edf3728d646eebd8dbb300e9168f4197
Tushar <153347682+Web-Tushar@users.noreply.github.com>
2024-08-24 10:07:16 +0600

    my first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <h3>tushar</h3>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
