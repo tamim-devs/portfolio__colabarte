5f6f1fa72105a6afca949f38ee4d1b33689d3713
tamim-devs <tamimh0727@gmail.com>
2024-08-24 12:09:14 +0600

    hoise

--- src/componant/home/Home.jsx
+++ src/componant/home/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import './Home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <section id='banner'>
+      <div className="container">
+        <div className="banner__wrapper">
+            <div className="banner__item">
+              
+            </div>
+            <div className="banner__img"></div>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
