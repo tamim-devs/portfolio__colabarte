bff3dbb10306f02ee7d8d91700581a125386fbf8
tamim-devs <tamimh0727@gmail.com>
2024-08-24 15:53:21 +0600

    bro

--- src/componant/home/Home.jsx
+++ src/componant/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css'
 import ban_profile from '../images/image 17.png'
+import { FaGithub, FaLinkedin, FaThreads } from 'react-icons/fa6'
 const Home = () => {
   return (
     <>
@@ -11,6 +12,11 @@ const Home = () => {
               <h1>Hi Iam</h1>
               <h2>Tushar Ahmed</h2>
               <p>I'm a Frontend Developer</p>
+            <div className="banner__social">
+              <div><FaThreads /></div>
+              <div><FaGithub /></div>
+              <div><FaLinkedin /></div>
+            </div>
             </div>
             <div className="banner__img">
               <img src={ban_profile} alt="" />
