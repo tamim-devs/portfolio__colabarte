d083487aad28545d093d1e1c4af1ab9ff91ebc3d
Tushar <153347682+Web-Tushar@users.noreply.github.com>
2024-08-25 22:20:30 +0600

    tushar

--- src/componant/home/Home.jsx
+++ src/componant/home/Home.jsx
@@ -9,17 +9,14 @@ const Home = () => {
       <div className="container">
         <div className="banner__wrapper">
             <div className="banner__item">
-              <h1>Hi Iam</h1>
-              <h2>Tushar Ahmed</h2>
+              <h3>Hi I am</h3>
+              <h1>Tushar Ahmed</h1>
               <p>I'm a Frontend Developer/Backend Developer</p>
+            </div>
             <div className="banner__social">
               <div><FaThreads /></div>
               <div><FaGithub /></div>
               <div><FaLinkedin /></div>
-            </div>
-            </div>
-            <div className="banner__img">
-              <img src={ban_profile} alt="" />
             </div>
         </div>
       </div>
--- src/layout/Navber.jsx
+++ src/layout/Navber.jsx
@@ -5,26 +5,26 @@ import navLogo from '../../src/componant/images/nav.png'
 const Navber = () => {
   return (
     <>
-    <div id="navbar">
-      <div className="container">
-        <div className="nav__wrapper">
-          <div className="nav__logo">
-            <img src={navLogo} alt="" />
-          </div>
-          <div className="nav__list">
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Skills</li>
-              <li>Portfolio</li>
-          <button className='nav__btn'>
-            Contact us
-          </button>
-            </ul>
+      <div id="navbar">
+        <div className="container">
+          <div className="nav__wrapper">
+            <div className="nav__logo">
+              <img src={navLogo} alt="" />
+            </div>
+            <div className="nav__list">
+              <ul>
+                <li >Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Portfolio</li>
+              </ul>
+              <button className='nav__btn'>
+                Contact us
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
